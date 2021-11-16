30a0d9cbd925ad070ef3b2b544fe70d86f5aaaf5
Adam Houston <phazyy9@gmail.com>
2021-11-16 19:50:53 +0000

    Creating component for balance category

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
-	import Navigation from './components/Navigation.svelte';
+	import Navigation from './components/Navigation.svelte'
+	import SmallCard from './components/SmallCard.svelte'
 	export let name: string;
 </script>
 
@@ -15,68 +16,22 @@
                     <div class="px-4 md:px-10 py-4 md:py-7 bg-gray-100 rounded-tl-lg rounded-tr-lg">
                         <div class="sm:flex items-center justify-between">
                             <p class="text-base sm:text-lg md:text-xl lg:text-2xl font-bold leading-normal text-gray-800">Address Overview</p>
-							<div class="flex items-center mt-4 sm:mt-0">
-								<div class="flex items-center pl-3 bg-white border rounded-md border-gray-200">
+							<div class="flex items-center mt-4 sm:mt-0 w-1/3">
+								<div class="flex items-center pl-3 w-full bg-white border rounded-md border-gray-200">
 									<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
 										<path d="M8.33333 13.1667C11.555 13.1667 14.1667 10.555 14.1667 7.33333C14.1667 4.11167 11.555 1.5 8.33333 1.5C5.11167 1.5 2.5 4.11167 2.5 7.33333C2.5 10.555 5.11167 13.1667 8.33333 13.1667Z" stroke="#9CA3AF" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round" />
 										<path d="M17.5 17.5L12.5 12.5" stroke="#9CA3AF" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round" />
 									</svg>
-									<input type="text" class="py-3 pl-1 w-full border-none focus:outline-none text-sm rounded-md text-gray-600 placeholder-gray-400" placeholder="Search" />
-								</div>
-								<div
-									class="ml-4 py-3 px-4 flex items-center text-sm font-medium leading-none text-gray-600 bg-gray-200 hover:bg-gray-300 cursor-pointer rounded">
-									<p class="text-gray-600 font-medium">Networks:</p>
-									<select class="focus:outline-none bg-transparent ml-1 ">
-										<option class="text-sm text-gray-600 font-medium ">All</option>
-										<option class="text-sm text-gray-600 font-medium ">Avalance</option>
-										<option class="text-sm text-gray-600 font-medium ">Ethereum</option>
-										<option class="text-sm text-gray-600 font-medium ">Fantom</option>
-									</select>
+									<input type="text" class="py-3 pl-1 border-none focus:outline-none text-sm rounded-md text-gray-600 placeholder-gray-400" placeholder="Search" />
 								</div>
 							</div>
                         </div>
                     </div>
-                    <div class="bg-white shadow px-4 md:px-10 pt-4 md:pt-7 pb-5 overflow-y-auto">
-						<div class="pt-3 pb-4 px-3 bg-white shadow rounded">
-							<p class="text-base font-medium leading-none text-gray-800">Wallet</p>
-							<div class="flex items-center justify-between mt-1">
-								<p class="text-xs leading-3 text-gray-600 ">Current Balance(s) Available</p>
-								<p class="text-base font-semibold leading-none text-indigo-700">$1,323</p>
-							</div>
-							<div class="w-full rounded-full mt-3.5 h-1.5 bg-indigo-50">
-								<div class="w-1/4 h-1.5 bg-indigo-500 rounded-full"></div>
-							</div>
-						</div>
-						<div class="pt-3 pb-4 px-3 bg-white shadow rounded mt-3">
-							<p class="text-base font-medium leading-none text-gray-800">Lending</p>
-							<div class="flex items-center justify-between mt-1">
-								<p class="text-xs leading-3 text-gray-600 ">Current Balance(s) Being Lent</p>
-								<p class="text-base font-semibold leading-none text-blue-500">$1,323</p>
-							</div>
-							<div class="w-full rounded-full mt-3.5 h-1.5 bg-blue-50">
-								<div class="w-2/3 h-1.5 bg-blue-500 rounded-full"></div>
-							</div>
-						</div>
-						<div class="pt-3 pb-4 px-3 bg-white shadow rounded mt-3">
-							<p class="text-base font-medium leading-none text-gray-800">Staking</p>
-							<div class="flex items-center justify-between mt-1">
-								<p class="text-xs leading-3 text-gray-600 ">Current Balance(s) Being Staked in Liquidity Pools</p>
-								<p class="text-base font-semibold leading-none text-green">$1,323</p>
-							</div>
-							<div class="w-full rounded-full mt-3.5 h-1.5 bg-green-50">
-								<div class="w-1/2 h-1.5 bg-green rounded-full"></div>
-							</div>
-						</div>
-						<div class="pt-3 pb-4 px-3 bg-white shadow rounded mt-3">
-							<p class="text-base font-medium leading-none text-gray-800">Debt</p>
-							<div class="flex items-center justify-between mt-1">
-								<p class="text-xs leading-3 text-gray-600 ">Current Balance(s) Being Borrowed</p>
-								<p class="text-base font-semibold leading-none text-pink-500">$1,323</p>
-							</div>
-							<div class="w-full rounded-full mt-3.5 h-1.5 bg-pink-50">
-								<div class="w-1/3 h-1.5 bg-pink-500 rounded-full"></div>
-							</div>
-						</div>
+					<div class="bg-white shadow px-4 md:px-10 pt-4 md:pt-7 pb-5 overflow-y-auto flex flex-row flex-wrap gap-4">
+						<SmallCard title="Wallet" desc="Balance(s) available of token(s)" amount="1875" accent="indigo" />
+						<SmallCard title="Lending" desc="Balance being provided as collateral" amount="2100" accent="blue" />
+						<SmallCard title="Staking" desc="Balance(s) provided in Liquidity Pools" amount="625" accent="green" />
+						<SmallCard title="Debt" desc="Borrowed Balance" amount="340" accent="pink" background="pink-100" />
                     </div>
                 </div>
             </div>
---
+++ src/components/SmallCard.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    export let title : string
+    export let desc : string
+    export let amount : string
+    export let accent : string
+    export let background : string = "white"
+</script>
+
+<div class="hover:shadow-lg p-4 bg-{background} rounded-lg border border-gray-200">
+    <p class="text-base font-medium leading-none text-gray-800">{title}</p>
+    <div class="flex items-center justify-between mt-1">
+        <p class="text-xs leading-3 text-gray-600 mr-4">{desc}</p>
+        <p class="text-base font-semibold leading-none text-{accent}-700">${amount}</p>
+    </div>
+    <div class="w-full rounded-full mt-3.5 h-1.5 bg-{accent}-50">
+        <div class="w-1/4 h-1.5 bg-{accent}-500 rounded-full"></div>
+    </div>
+</div>
